40f786e9dd5f2a49bcfdd2166851ceafd15d6451
Color Pc <phamtruongtan0903@gmail.com>
2023-06-21 09:41:24 +0700

    commit

--- app/config/passport.js
+++ app/config/passport.js
@@ -1,12 +1,13 @@
 require('dotenv').config();
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
+const url = 'http://localhost:'+process.env.PORT+'/auth/google/callback';
 module.exports = (passport) => {
   
   passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: 'http://localhost:8080/auth/google/callback'
+    callbackURL: url
   },
   (accessToken, refreshToken, profile, done) => {
     return done(null, profile);
